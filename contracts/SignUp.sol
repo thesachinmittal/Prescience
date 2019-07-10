e360e6d1954cb87ab47998d979848d524e5a3b68
sanchaymittal <sanchaymittal@gmail.com>
2019-07-11 01:39:41 +0530

    Update SignUp.

--- contracts/SignUp.sol
+++ contracts/SignUp.sol
@@ -5,35 +5,95 @@ contract SignUp {
 
   ///@notice Owner's address
   address public owner;
+  string username;
 
-  struct Hero{
-    string[12] username;
+  struct Account{
     address account;
+    bool access;
   }
-
-  mapping (uint => Hero) Heroes;
+  mapping (string => Account) Hero;
 
   function() external payable{
     revert("Please try Again");
   }
 
+
+  //
+  //Events
+  //
+  event SuccesfulRegistration(string indexed _username);
+
+  //
+  //Modifiers
+  //
+
+  ///@notice Checks the owner of the contract
+  modifier checkOwner(){
+    require(msg.sender == owner,"Owner's Permission");
+    _;
+  }
+
+  modifier verifyName(string memory _username){
+    require (!Hero[_username].access," Account Name Exists");
+    _;
+  }
+
+  ///@notice verify the transaction caller using address.
+  modifier verifyCaller (address _address){
+    require (msg.sender == _address, "This one is a Registered Account");
+    _;
+  }
+
+  ///@notice Refund the excess amount
+  modifier checkValue(uint threshold) {
+    _;
+    uint amountToRefund = msg.value - threshold;
+    msg.sender.transfer(amountToRefund);
+  }
+
+
+  //
+  //Constructor
+  //
+
   constructor() public {
     owner = msg.sender;
   }
 
-  event SuccesfulRegistration(string indexed _username);
-  
-  modifier checkAccountName(string memory _username){
-    _;
-  }
-  
 
+
+  //
+  //Functions
+  //
 
   ///@notice Addaccount registers new user to the platform
   ///@dev Register's Account which includes username(Character limit 12), Ethereum address
   /* Account should made a threshold token transaction of JL tokens. */
-  ///@param _username identification of user.
+  ///@param _name identification of user.
   ///@param _address Ethereum Address of the user.
-  function addAccount(string memory _username,address _address) public returns(bytes32) {  
+  function addAccount(string memory _name,address _address) public returns(bytes32){
+    uint threshold = 100;
+    entryAmount(threshold);
+    require(!Hero[_name].access,"Hero Exist, Please Choose another name");
+    Hero[_name].account = _address;
+    Hero[_name].access = true;
+  }
+
+
+  //
+  //Helper Functions
+  //
+
+  ///@notice Entry Security deposit for interaction with Judgement Platform.
+  ///@dev
+  ///@param threshold minimum amount required to interact with platform.
+  ///@return Success report
+  function entryAmount(uint threshold)
+  private
+  checkValue(threshold)
+  returns(bool){
+    require(msg.value > threshold, "Funds Insufficient");
+    msg.sender.transfer(threshold);
+
   }
 }
