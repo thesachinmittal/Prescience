a4d295fedf813ee473910d3cb2c155f58d276a61
sanchaymittal <sanchaymittal@gmail.com>
2019-07-17 14:06:09 +0530

    Create Escrow Contract

---
+++ contracts/Escrow.sol
@@ -0,0 +1,11 @@
+///@author Sanchay Mittal
+///@dev Create an Escrow Account contains the details and timely execution.
+pragma solidity ^0.5.0;
+
+
+contract Escrow {
+  constructor() public {
+  }
+
+  
+}
