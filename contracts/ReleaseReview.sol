c692a87e39ab35785de0cc547c86400d14df9aac
sanchaymittal <sanchaymittal@gmail.com>
2019-07-19 07:53:01 +0530

    Review token Created and updated.

--- contracts/ReleaseReview.sol
+++ contracts/ReleaseReview.sol
@@ -1,13 +1,11 @@
 ///@author Sanchay Mittal
 ///@dev Reviews Time Lock
-
-
 pragma solidity ^0.5.0;
 
+import "./ReviewToken.sol";
 
-contract TimeLockReview{
+contract ReleaseReview{
 
- address public beneficiary;
  uint256 public releaseTime;
 
   constructor (uint256 _releaseTime)
@@ -17,11 +15,10 @@ contract TimeLockReview{
   }
 
   function release() public {
-    require(block.timestamp >= releaseTime);
-
-    uint256 amount = token.balanceOf(address(this));
-    require(amount > 0);
-
-    token.transfer(beneficiary, amount);
+    require(block.timestamp >= releaseTime, " Too Soon..");
+    for( uint i = 0 ; i < (leaders.length-1) ; i++ )
+    {
+      getNFT(id);
+    }
   }
 }
