1d31e902facd92aafeaee4103834ac7270855439
sanchaymittal <sanchaymittal@gmail.com>
2019-07-21 14:26:19 +0530

    Voting Contract Update.

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,32 +2,27 @@ pragma solidity ^0.5.0;
 
 import "./ReleaseReview.sol";
 
-contract Crowd{
+contract Voting{
   // The two choices for your vote
-  struct vote{
+  struct vote {
     uint UpNonTechnical;
     uint DownNonTechnical;
     uint UpTechnical;
     uint DownTechnical;
     bool choice;
   }
-  
-  // string public choice1;
-  // string public choice2;
 
   // Information about the current status of the vote
-    uint public votesForChoice1;
-    uint public votesForChoice2;
-    uint public commitPhaseEndTime;
-    bytes32 votePassword;
+  uint public commitPhaseEndTime;
+  bytes32 votePassword;
 
-    enum Status{
-      Committed, Revealed, Voted
-    }
+  enum Status{
+    Open, Committed, Revealed
+  }
 
-    // The actual votes and vote commits
-    bytes32[] public voteCommits;
-    mapping(bytes32 => Status) voteStatuses; // Either `Committed` or `Revealed`
+  // The actual votes and vote commits
+  bytes32[] public voteCommits;
+  mapping(bytes32 => Status) voteStatuses; // Either `Committed` or `Revealed`
 
     // Events used to log what's going on in the contract
     event logString(string);
@@ -35,13 +30,9 @@ contract Crowd{
     event voteWinner(string, string);
 
     // Constructor used to set parameters for the this specific vote
-    constructor(
-      uint _commitPhaseLengthInSeconds,
-      string _choice1,
-      string _choice2) public{
+    constructor(uint _commitPhaseLengthInSeconds)
+      public{
         commitPhaseEndTime = block.timestamp + _commitPhaseLengthInSeconds;
-        choice1 = _choice1;
-        choice2 = _choice2;
     }
 
     function commitVote(bytes32 _voteCommit) public{
@@ -49,7 +40,7 @@ contract Crowd{
 
         // Check if this commit has been used before
         Status status = voteStatuses[_voteCommit];
-        require(status == Committed);
+        require(status == Open);
 
         // We are still in the committing period & the commit is new so add it
         voteCommits.push(_voteCommit);
@@ -57,22 +48,22 @@ contract Crowd{
         emit newVoteCommit("Vote committed with the following hash:", _voteCommit);
     }
 
-    function revealVote(string _vote, bytes32 _voteCommit) public{
-        require(block.timestamp > commitPhaseEndTime); // Only reveal votes after committing period is over
+    function revealVote(uint upTechnical,
+      uint downTechnical,
+      uint upNonTechnical,
+      uint downNonTechnical,
+      uint choice,
+      uint secretPassword)
+       public{
+        require(block.timestamp > commitPhaseEndTime, "Please Only reveal votes after committing period is over" );
 
         // FIRST: Verify the vote & commit is valid
-        bytes memory bytesVoteStatus = bytes(voteStatuses[_voteCommit]);
-        if (bytesVoteStatus.length == 0) {
-            emit logString('A vote with this voteCommit was not cast');
-        } else if (bytesVoteStatus[0] != 'C') {
-            emit logString('This vote was already cast');
-            return;
-        }
+        Status status = voteStatuses[_voteCommit];
 
-        if (_voteCommit != keccak256(_vote)) {
-            emit logString('Vote hash does not match vote commit');
-            return;
-        }
+        require(status == Committed, " Vote Wasn't committed yet");
+
+        require(_voteCommit !=
+        keccak256(upTechnical, downTechnical, upNonTechnical, downNonTechnical, choice, secretPassword),'Vote hash does not match vote commit');
 
         // NEXT: Count the vote!
         bytes memory bytesVote = bytes(_vote);
