9d88aec78c493d2152e212ee5fdfe43f69275a77
sanchaymittal <sanchaymittal@gmail.com>
2019-07-22 14:41:00 +0530

    Phase Setup

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -12,7 +12,9 @@ contract Voting{
     mapping (uint => uint) Choice;
 
   // Information about the current status of the vote
+  uint public reviewPhaseEndTime;
   uint public commitPhaseEndTime;
+  uint public revealPhaseEndTime;
 
   enum Status{
     Committed, Revealed
@@ -28,32 +30,33 @@ contract Voting{
     event voteWinner(string, string);
 
     // Constructor used to set parameters for the this specific vote
-    constructor(uint _commitPhaseLengthInSeconds)
+    constructor(uint _ReviewPhaseLengthInSeconds, uint _CommitPhaseLengthInSeconds, uint _RevealPhaseLengthInSeconds)
       public{
-        commitPhaseEndTime = block.timestamp + _commitPhaseLengthInSeconds;
+        reviewPhaseEndTime = block.timestamp + _ReviewPhaseLengthInSeconds;
+        commitPhaseEndTime = block.timestamp + _CommitPhaseLengthInSeconds + _ReviewPhaseLengthInSeconds;
+        revealPhaseEndTime = block.timestamp + _RevealPhaseLengthInSeconds + _CommitPhaseLengthInSeconds + _ReviewPhaseLengthInSeconds;
     }
 
     function commitVote(bytes32 _voteCommit) public{
+      require(block.timestamp > reviewPhaseEndTime, "Wait for review period to end ");
       require(block.timestamp < commitPhaseEndTime, "Only allow commits during committing period");
 
-        // // Check if this commit has been used before
-        // Status status = voteStatuses[_voteCommit];
-        // require(status == Open);
-
-        // We are still in the committing period & the commit is new so add it
+      // We are still in the committing period & the commit is new so add it
       voteCommits[msg.sender] = _voteCommit;
       voteStatuses[_voteCommit] = Status.Committed;
-        emit newVoteCommit("Vote committed with the following hash:", _voteCommit);
+      emit newVoteCommit("Vote committed with the following hash:", _voteCommit);
     }
 
-    function revealVote(uint upTechnical,
+    function revealVote(
+      uint upTechnical,
       uint downTechnical,
       uint upNonTechnical,
       uint downNonTechnical,
       uint choice,
-      string memory secretPassword)
-       public{
+      string memory salt)
+      public{
         require(block.timestamp > commitPhaseEndTime, "Please Only reveal votes after committing period is over");
+        require(block.timestamp < revealPhaseEndTime, " Only allowed During the reveal period");
 
         // FIRST: Verify the vote & commit is valid
         bytes32 _voteCommit = voteCommits[msg.sender];
@@ -62,8 +65,8 @@ contract Voting{
         require(status == Status.Committed, " Vote Wasn't committed yet");
 
         require(_voteCommit !=
-        keccak256(abi.encodePacked(upTechnical, downTechnical, upNonTechnical, downNonTechnical, choice, secretPassword)),'Vote hash does not match vote commit');
-        
+        keccak256(abi.encodePacked(upTechnical, downTechnical, upNonTechnical, downNonTechnical, choice, salt)),'Vote hash does not match vote commit');
+
 
         // NEXT: Count the vote!
         ++UpTechnical[upTechnical];
@@ -73,5 +76,10 @@ contract Voting{
         ++Choice[choice];
         voteStatuses[_voteCommit] = Status.Revealed;
     }
-    
+
+    function count() view public{
+      require(block.timestamp > revealPhaseEndTime,"Let the reveal Period end first");
+      // Final Phase started
+      
+    }
 }
